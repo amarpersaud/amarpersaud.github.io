e94dc6086fd4fc451f1339d28584b7f374e11f42
Teh_Sparta <tehspartaa@gmail.com>
2015-05-10 15:35:23 -0400

    test2

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,4 +1,4 @@
-  createCookie('maincolor', 'default', 10);
+
  function setpercent(x, id, percent,tdelay){
       ttime = tdelay / (percent-x);
       document.getElementById(id).style.width = percent + "%";
@@ -49,8 +49,14 @@
 }
 
 function readCookie(name) {
-  var value = document.cookie;
-  return value;
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
 }
 
 function eraseCookie(name) {
@@ -62,6 +68,8 @@ function setButton(color){
 }
 
 
+if(! readCookie('maincolor')){
+  createCookie('maincolor', 'default', 10);
   var color = readCookie('maincolor');
   alert(color);
   
