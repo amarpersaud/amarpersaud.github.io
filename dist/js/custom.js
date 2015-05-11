2ba1b433db9b91539e235c91a2247b6082252bd9
Teh_Sparta <tehspartaa@gmail.com>
2015-05-11 17:58:30 -0400

    Cookie fix 2

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -42,7 +42,6 @@
 		var date = new Date();
 		date.setTime(date.getTime()+(days*24*60*60*1000));
 		var expires = "; expires="+date.toGMTString();
-	else var expires = "";
 	document.cookie = name+"="+value+expires+"; path=/";
 }
 
