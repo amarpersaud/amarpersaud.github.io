681b2b285907bea9eba8915c9d72000e4e624214
Teh_Sparta <tehspartaa@gmail.com>
2015-05-11 17:56:46 -0400

    cookie broken

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -39,11 +39,9 @@
 
     
     function createCookie(name,value,days) {
-	if (days) {
 		var date = new Date();
 		date.setTime(date.getTime()+(days*24*60*60*1000));
 		var expires = "; expires="+date.toGMTString();
-	}
 	else var expires = "";
 	document.cookie = name+"="+value+expires+"; path=/";
 }
