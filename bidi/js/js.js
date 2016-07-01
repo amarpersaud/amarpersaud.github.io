c46779e696b80b1b413ce64a63911d66bac248c0
Amar Persaud <tehspartaa@gmail.com>
2016-07-01 17:56:05 -0400

    navbar import + cookie js

--- bidi/js/js.js
+++ bidi/js/js.js
@@ -43,3 +43,27 @@ function GetURLParameter(sParam)
   searchbutton.onclick = function(){
     search(searchbar.value);
   }
+  
+  function createCookie(name,value) {
+    var date = new Date ();
+    date.setYear (date.getYear() + 100);
+    date = date.toGMTString();
+    
+		var expires = "; expires="+date;
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+
+  function eraseCookie(name) {
+    createCookie(name,"",-1);
+  }
