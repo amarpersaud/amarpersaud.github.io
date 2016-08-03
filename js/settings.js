157ce193430304a4886095b875127e667f322c70
Amar Persaud <tehspartaa@gmail.com>
2016-08-03 01:18:00 -0400

    Tooltip + Demo background + testserver

--- js/settings.js
+++ js/settings.js
@@ -5,9 +5,10 @@
   newval = document.querySelectorAll('[data-value="'+document.main_color+'"]')[0];
   colordropdown.value = newval.value;
   colorbutton.innerHTML = newval.innerHTML;
-  document.getElementById("loading").parentNode.removeChild(document.getElementById("loading"));
   function setmaincolor(e){ maincolor(e.currentTarget.value);}
   
   function saveSettings(){
     createCookie("maincolor", colordropdown.value);
   }
+  
+  document.getElementById("loading").parentNode.removeChild(document.getElementById("loading"));
--- project/rangeslider/js/script.js
+++ project/rangeslider/js/script.js
@@ -1,7 +1,13 @@
+//change event for slider value changed
+
+var changedEvent = new Event('changed');
+//Clamp
 Number.prototype.clamp = function(min, max) {
   return Math.min(Math.max(this, min), max);
 };
- var mouseDown = 0;
+
+//check if the mouse is pressed to prevent multiple sliders from moving at once and make the slider only move if the mouse is pressed.
+  var mouseDown = 0;
   document.body.onmousedown = function(evt) { 
     if(evt.button==0){
       mouseDown = 1;
@@ -13,8 +19,11 @@ Number.prototype.clamp = function(min, max) {
     }
   }
 
+//get the range inputs
 rs = document.getElementsByClassName('rangeinput');
+//the currently selected range input
 currentrange = rs[0];
+
 for (i = 0; i < rs.length; i++) { 
   var range = rs[i]; 
   if(range.getAttribute("value") != ""){
@@ -31,6 +40,7 @@ for (i = 0; i < rs.length; i++) {
       progress = e.currentTarget.getElementsByTagName("DIV")[0];
       progress.style.width = (X / e.currentTarget.offsetWidth * 100).clamp(0, 100) + "%";   
       e.currentTarget.setAttribute("value", (X / e.currentTarget.offsetWidth * 100).clamp(0, 100));
+      e.currentTarget.dispatchEvent(changedEvent);
     }
   }      
   range.onmousedown = function(e) { 
@@ -40,5 +50,6 @@ for (i = 0; i < rs.length; i++) {
     progress = e.currentTarget.getElementsByTagName("DIV")[0];
     progress.style.width = (X / e.currentTarget.offsetWidth * 100).clamp(0, 100) + "%";
     e.currentTarget.setAttribute("value", (X / e.currentTarget.offsetWidth * 100).clamp(0, 100));    
+    e.currentTarget.dispatchEvent(changedEvent);
   }
 }
